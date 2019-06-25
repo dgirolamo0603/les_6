c3df3b64fdc6844431e3fc313885ceec4327464a
Alex <dgirolamo777@gmail.com>
2019-06-25 15:04:38 +0300

    "all9"

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PostListItem from '../post-list-item';
+import './post-list.css';
+
+const PostList=({posts, onDelete})=>{
+
+    const elements=posts.map((item)=>{
+        const {id, ...itemProps}=item;
+        return(
+            <li key={id} className="list-group-item">
+                  <PostListItem 
+                  {...itemProps}
+                  onDelete={()=>onDelete(id)}/>
+            </li>
+        )
+    });
+    return (
+        <ul className="app-list list-group">
+            {elements}
+            {/* <PostListItem label='That is so good'/> 
+            <PostListItem label='I need s break'/> */}
+        </ul>
+    )
+}
+ export default PostList;
+
+
+
+
