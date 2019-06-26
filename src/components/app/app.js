8d76e7b2ff02c7150c0c61cdb5f2cdf8c2c79fdb
Alex <dgirolamo777@gmail.com>
2019-06-26 17:09:27 +0300

    "all_5"

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,13 +14,17 @@ export default class App extends React.Component {
         {label: 'Going to learn React', important: true,like:false, id:1},
         {label: 'That is so good', important: false,like:false, id:2},
         {label: 'I need s break', important: false,like:false, id:3}
-                                ]
+                                ],
+                        term: '',
+                        filter:'all'
                 };
                 this.deleteItem=this.deleteItem.bind(this);
                 this.addItem = this.addItem.bind(this);
                 this.maxId=4;
                 this.onToggleImportant=this.onToggleImportant.bind(this);
                 this.onToggleLiked=this.onToggleLiked.bind(this);
+                 this.onUpdateSearch = this.onUpdateSearch.bind(this);
+                 this.onFilterSelect = this.onFilterSelect.bind(this);
 
         }
 
@@ -47,7 +51,17 @@ export default class App extends React.Component {
         }
 
         onToggleImportant(id){
-                console.log(`Important ${id}`);
+                   this.setState(({data})=>{
+                       const index=data.findIndex(elem=>elem.id===id);
+
+                       const old=data[index];
+                       const newItem={...old,important:!old.important};
+
+                       const newArr = [...data.slice(0, index),newItem, ...data.slice(index + 1)];
+                       return {
+                               data:newArr
+                       };
+               });
         }
         onToggleLiked(id){
                this.setState(({data})=>{
@@ -59,33 +73,56 @@ export default class App extends React.Component {
                        const newArr = [...data.slice(0, index),newItem, ...data.slice(index + 1)];
                        return {
                                data:newArr
-                       }
-               }) 
+                       };
+               });
+        }
+
+
+
+        searchPost(items,term){
+                if(term.length===0){
+                        return items
+                }
+              return  items.filter((item)=>{
+                        return item.label.indexOf(term)>-1;
+              });
+        }
+
+
+        filterPost(items, filter){
+                if(filter==='like'){
+                        return items.filter(item=>item.like)
+                }else {
+                        return items
+                }
+        }
+
+        onUpdateSearch(term){
+                this.setState({term});
+        }
+        onFilterSelect(filter){
+                this.setState({filter})
         }
         render(){
-                const{data}=this.state;
-                const liked=data.filter(item=>item.like).length;
+                const{data, term,filter}=this.state;
+                const liked=data.filter(item =>item.like).length;
                 const allPosts=data.length;
-        return ( 
-    <div className="app"> 
-    < AppHeader
-    liked={liked}
-    allPosts={allPosts}/>
-            <div className="search-panel d-flex">
-            <SearchPanel/>
-            <PostStatus/>
-            </div>
-            <PostList 
-            posts={this.state.data}
-            onDelete={this.deleteItem}
-            onToggleImportant={this.onToggleImportant}
-            onToggleLiked={this.onToggleLiked}/>
-
-            <PostAddForm
-            onAdd={this.addItem}/>
-    </div>
+                const visiblePosts=this.filterPost(this.searchPost(data,term), filter);
+        
+        return (
+                <div className='app'>
+                        <AppHeader liked={liked} allPosts={allPosts}/>
+                        <div className='search-panel d-flex'>
+                                <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                                <PostStatus filter={filter} 
+                                onFilterSelect={this.onFilterSelect}/>
+                        </div>
+                        <PostList posts={visiblePosts} onDelete={this.deleteItem} onToggleImportant={this.onToggleImportant} 
+                        onToggleLiked={this.onToggleLiked}/>
+                        <PostAddForm onAdd={this.addItem}/>
+                </div>
         )
-}
-   
+                }
+        
+        }
 
-}
